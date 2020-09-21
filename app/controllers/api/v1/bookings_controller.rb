6c4ce2d2113189f311cdae9a81115250970f2b11
Ziad <ziad1991@hotmail.co.uk>
2020-09-21 14:06:33 +0100

    Add API functionality to create, read, update & destroy spaces

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,6 +1,11 @@
 module Api
     module V1
         class BookingsController < ApplicationController
+            def index
+                bookings = Booking.all
+
+                render json: BookingSerializer.new(bookings).serialized_json
+            end
         end
     end
 end
--- app/controllers/api/v1/spaces_controller.rb
+++ app/controllers/api/v1/spaces_controller.rb
@@ -1,6 +1,54 @@
 module Api
     module V1
         class SpacesController < ApplicationController
+            def index
+                spaces = Space.All
+
+                render json: SpaceSerializer.new(spaces).serialized_json
+            end
+
+            def show
+                spaces = Spaces.find_by(id: params[:id])
+
+                render json: SpaceSerializer.new(spaces).serialized_json
+            end
+
+            def create
+                space = Space.new(spaces_params)
+
+                if space.save
+                    render json: SpaceSerializer.new(space).serialized_json
+                else
+                    render json: {error: space.errors.messages }, status: 422
+                end
+            end
+
+            def update
+                space = Space.find_by(id: params[:id])
+
+                if space.update(spaces_params)
+                    render json: SpaceSerializer.new(space).serialized_json
+                else
+                    render json: {error: space.errors.messages }, status: 422
+                end
+            end
+
+            def destroy
+                space = Space.find_by(id: params[:id])
+
+                if airline.destroy
+                    head :no_content
+                else 
+                    render json: { error: airline.errors.messages }, status: 422
+                end
+            end
+            
+
+            private
+            
+            def spaces_params
+                params.require(:space).permit(:name, :description, :price, :image_url, :available_from, :available_to)
+            end
         end
     end
 end
