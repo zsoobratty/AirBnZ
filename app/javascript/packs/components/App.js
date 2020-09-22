ae545482430cc2ae791107426caa4765bcfa4be0
Ziad <ziad1991@hotmail.co.uk>
2020-09-22 15:41:27 +0100

    Add base styling to Spaces

--- app/javascript/packs/components/App.js
+++ app/javascript/packs/components/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './Home/Home'
+import '../main.css'
 
 const App = () => {
 
--- app/javascript/packs/components/Home/Space.js
+++ app/javascript/packs/components/Home/Space.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import '../../styles/Space.css'
 
 const Space = ({space}) => {
     console.log(space)
     return (
-        <div>
-            <h1>{space.id}</h1>
-            <h2>{space.attributes.name}</h2>
-            <img src={space.attributes.img_url} alt={space.attributes.description}/>
+        <div className='Space'>
+            <div className="Space-card">
+                <h2>{space.attributes.name} - £{space.attributes.price} per night</h2>
+                <img src={space.attributes.img_url} alt={space.attributes.description}/>
+                <p>{space.attributes.description}</p>
+                <p>Available from {space.attributes.available_from} to {space.attributes.available_to} </p>
+            </div>
         </div>
     )
 }
